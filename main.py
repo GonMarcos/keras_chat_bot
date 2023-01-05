0462468374a1e1444a7a1354d2287208e831689e
justindd1994 <justindd1994@gmail.com>
2023-01-05 12:53:23 +0200

    Fixed issues and did some clean up.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     if sys.argv[1] == "train":
-        import src.trainning.trainning_model
+        import src.training.training_model
 
 if __name__ == "__main__":
     main()
--- src/training/training_model.py
+++ src/training/training_model.py
@@ -6,7 +6,7 @@ import pickle
 
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
 import random
 
@@ -14,7 +14,7 @@ words=[]
 classes = []
 documents = []
 ignore_words = ['?', '!', '=']
-data_file = open('src/trainning/intents.json').read()
+data_file = open('src/training/intents.json').read()
 intents = json.loads(data_file)
 
 for intent in intents['intents']:
@@ -35,15 +35,9 @@ words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
-# classes = intents
-print (len(classes), "classes", classes)
-# words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
 
-pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
+pickle.dump(words,open('src/trained_data/words.pkl', 'wb'))
+pickle.dump(classes,open('src/trained_data/classes.pkl', 'wb'))
 
 # create our training data
 training = []
@@ -74,4 +68,21 @@ train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-import ai_brain
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model 
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('src/trained_data/chatbot_model.h5', hist)
+
+print("model created")
